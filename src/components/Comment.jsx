09c9f6614a4a375404530a3723f5b17344fefcef
PauloGodoiS <paulogodoi.sp@gmail.com>
2022-03-21 19:18:21 -0300

    Projeto Criado e Comment Component criado

---
+++ src/components/Comment.jsx
@@ -0,0 +1,12 @@
+import './Comment.css'
+
+const Comment = () => {
+    return (
+        <div className="comment">
+            <h2>Paulo Godoi</h2>
+            <p>Aqui é o meu Comment Component.</p>
+        </div>
+    )
+}
+
+export default Comment
